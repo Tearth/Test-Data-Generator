007e3194a88fe49e8068cf09df332a8447b54dc6
tearth <tearthdev@gmail.com>
2019-12-07 13:44:41 +0100

    Add command line arguments.

---
+++ TestDataGenerator/Options.cs
@@ -0,0 +1,31 @@
+﻿using CommandLine;
+
+namespace TestDataGenerator
+{
+    public class Options
+    {
+        [Option("output", HelpText = "Set directory where output files will be saved.", Required = false, Default = "./output")]
+        public string OutputPath { get; set; }
+
+        [Option("count", HelpText = "Set number of the sets which will be generated.", Required = false, Default = 10)]
+        public int TestSetsCount { get; set; }
+
+        [Option("min-tasks", HelpText = "Set number of the minimum tasks in the set.", Required = false, Default = 1)]
+        public int MinTasksCount { get; set; }
+
+        [Option("max-tasks", HelpText = "Set number of the maximum tasks in the set.", Required = false, Default = 10)]
+        public int MaxTasksCount { get; set; }
+
+        [Option("min-cpu", HelpText = "Set number of the minimum processors in the set.", Required = false, Default = 1)]
+        public int MinProcessorsCount { get; set; }
+
+        [Option("max-cpu", HelpText = "Set number of the maximum processors in the set.", Required = false, Default = 4)]
+        public int MaxProcessorsCount { get; set; }
+
+        [Option("min-time", HelpText = "Set minimum time which will be assigned to the task.", Required = false, Default = 1)]
+        public int MinTaskTime { get; set; }
+
+        [Option("max-time", HelpText = "Set maximum time which will be assigned to the task.", Required = false, Default = 100)]
+        public int MaxTaskTime { get; set; }
+    }
+}
--- TestDataGenerator/Program.cs
+++ TestDataGenerator/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using CommandLine;
 
 namespace TestDataGenerator
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test Data Generator");
+            Console.WriteLine("===================");
+            Console.WriteLine();
+
+            Parser.Default.ParseArguments<Options>(args).WithParsed(options =>
+            {
+                Console.WriteLine($"Output path: {options.OutputPath}");
+                Console.WriteLine($"Test sets count: {options.TestSetsCount}");
+                Console.WriteLine($"Minimum tasks count: {options.MinTasksCount}");
+                Console.WriteLine($"Maximum tasks count: {options.MaxTasksCount}");
+                Console.WriteLine($"Minimum processors count: {options.MinProcessorsCount}");
+                Console.WriteLine($"Maximum processors count: {options.MaxProcessorsCount}");
+                Console.WriteLine($"Minimum task time: {options.MinTaskTime}");
+                Console.WriteLine($"Maximum task time: {options.MaxTaskTime}");
+            });
+
+            Console.WriteLine();
+            Console.WriteLine("===================");
+            Console.WriteLine();
+            Console.Read();
         }
     }
 }
